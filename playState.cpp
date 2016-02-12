c414d05c0e96798717099371e670b9f91422fc9a
Corithian Williams <corithianmw96@gmail.com>
2016-02-11 22:35:08 -0500

    Just more messing around with the test code

--- playState.cpp
+++ playState.cpp
@@ -24,14 +24,6 @@ PlayState::PlayState(Window* w) : manager{}, window(w), moveSys{&manager, 6000},
         \nstop_\ncreate %self component:bounds block 0 0 320 480 \
         \nstop_\ncreate %self component:collider Player Point 0 0 \
         \nstop_ @onUpdate\n end_script"), 0);
-    manager.addComponent(std::string("component:script 2000 \
-        @onStart\
-        \ncreate %self component:position 0, 0\
-        \nstop_\ncreate %self\ncomponent:velocity 60, 35\
-        \nstop_\ncreate %self\ncomponent:sprite NamelessSheet 0, 0\
-        \nstop_\ncreate %self component:bounds destroy 0 0 320 480 \
-        \nstop_\ncreate %self component:collider Enemy AABB  0 0 -10 =10 10 10 \
-        \nstop_ @onUpdate\n end_script"), 1);
 }
 
 void PlayState::handleInput() {
